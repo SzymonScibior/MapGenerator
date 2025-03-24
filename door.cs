b686fa113d9a37f61a4d0170a3584308cc9be2be
Szymon Scibior <szymonscibior1@gmail.com>
2025-03-24 18:58:12 +0100

    isUsed field added
    Added field to implement deleting doors which has not been used

--- door.cs
+++ door.cs
@@ -6,5 +6,8 @@ public class Door : MonoBehaviour
 {
     // Set this in the Inspector for each door.
     public DoorDirection doorDirection;
+    // Set to true when this door connects two rooms.
+    public bool isUsed = false; 
+
 }
 
